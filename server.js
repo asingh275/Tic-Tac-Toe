0740c30a0b75ba972bc1532cc7adebf6c7ed1877
gustavocss <gcarvalhaesdes00@mylangara.ca>
2022-07-22 15:46:36 -0800

    Added socketio to server

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
-const server = app.listen(process.env.PORT || 3000, ()=>console.log("Listening on 8080"));
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("new connection");
+})
+
+server.listen(process.env.PORT || 3000, ()=>console.log("Listening on 3000"));
 
 app.use(express.static("public"));
 app.use(express.json());
