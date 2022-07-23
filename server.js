196701f52f28cfc835241265ca971d6f43374b70
gustavocss <gcarvalhaesdes00@mylangara.ca>
2022-07-22 20:56:41 -0800

    Changed io server

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
-const server = http.createServer(app);
+const cors = require("cors");
 
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-    console.log("new connection");
-})
-
-server.listen(process.env.PORT || 3000, ()=>console.log("Listening on 3000"));
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+})
