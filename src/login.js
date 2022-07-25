8f0b6b6cbc032c1e6468177c4b99b013480068bf
gustavocsak <gustavocsak@gmail.com>
2022-07-24 20:23:06 -0800

    Modified styles for login page

--- src/login.js
+++ src/login.js
@@ -25,53 +25,43 @@ const Login = (props) =>{
     }
     return (
         <>
-            <div className="px-4 py-5 text-center">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-6 mb-5 mb-lg-0">
-                            <h1>App title here</h1>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                            olestias laudantium exercitationem quam illum earum totam,
-                            commodi a alias, distinctio adipisci dolore tempora. Aperiam,
-                            laborum eius expedita maxime unde laboriosam reprehenderit.</p>
+            <div className="px-4 py-5 text-center h-100">
+                <div className="container shadow rounded mt-5">
+                    <div className="row  ">
+                        <div className="col md-6 p-0 text-start">
+                           <div className="card left bg-dark">
+                                <div className="card-body px-4 py-5 text-white">
+                                    <h1>App title</h1>
+                                    <h3>Subheader</h3>
+                                    <p className="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                    olestias laudantium exercitationem quam illum earum totam,
+                                    commodi a alias, distinctio adiasdasdaasdasdpisci dolore tempora. Aperiam,
+                                    laborum eius expedita maxime unde laboriosaasdasdasdasdasdasdasdasdm reprehenderit.</p>
+                                </div>
+                                
+                          </div>
+                            
                         </div>
-                        <div className="col-lg-6 mb-5 mb-lg-0">
-                            <div className="card">
+                        <div className="col md-6 p-0">
+                            <div className="card right">
                                 <div className="card-body px-4 py-5">
                                     <div className="row px-4 text-start">
-                                        <h2>Welcome</h2>
+                                        <h3>Welcome</h3>
                                     </div>
                                     <div className="row px-4 mb-3 text-start">
                                         <p className="lead">Log in to continue</p>
                                     </div>
 
-                                    <div className="row">
-                                        <form action="">
-                                            <div className="row">
-                                                <div class="form-floating my-3 px-4">
-                                                    <input type="email" class="form-control" id="floatingInput" placeholder="email" />
-                                                    <label className="ms-4" for="floatingInput">Email address</label>
-                                                </div>
-                                            </div>
-                                            <div className="row">
-                                                <div class="form-floating mt-2 mb-3 px-4">
-                                                    <input type="password" class="form-control" id="floatingPassword" placeholder="password" />
-                                                    <label className="ms-4" for="floatingInput">Password</label>
-                                                </div>
-                                            </div>
-                                            <div className="row px-4 mt-2">
-                                                <button class="btn btn-primary btn-lg" type="submit">Log in</button>
-                                            </div>
-                                        </form>
+                                    
 
-                                    </div>
-
-                                    <div className="row px-2 mt-5 google-login">
+                                    <div className="row px-2 mt-3 google-login">
                                         <GoogleLogin
                                             onSuccess={onSuccess}
                                             onError={onFailure}
                                             size={"large"}
                                             width={400}
+                                            theme={"filled_black"}
+                                            
                                          />
                                     </div>
                                     
