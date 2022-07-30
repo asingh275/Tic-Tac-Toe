bb984e64dc62d8184d2ab0ef44b5bb46b375efb8
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-07-29 19:18:50 -0700

    Fixing email api

--- controller/emailController.js
+++ controller/emailController.js
@@ -8,7 +8,7 @@ const sendEmail = (req, res, next) => {
   const { to, gameID, userName } = req.body;
   const msg = {
     to: { email: to },
-    from: "cedafiso@gmail.com",
+    from: "asingh275@mylangara.ca",
     subject: "Game Invitation to Let's Play!",
     text: `${userName} has invited you to play a game. Here is the game ID ${gameID}.`,
     html: `<div style="background-color:black;color:white;height:200px"><h1 style="text-align:center;">Let's play</h1><strong>${userName} has invited you to play a game. Here is the game ID ${gameID}.</strong></div>`,
@@ -16,12 +16,19 @@ const sendEmail = (req, res, next) => {
   sgMail
     .send(msg)
     .then((result) => {
-      console.log(result);
-      res.send("Moral");
+      res.json({
+        message: "Email sent successfully",
+        from: msg.from,
+        to: msg.to,
+      }).status(200);
     })
     .catch((error) => {
-      console.log(error);
-      res.send("Cagaste");
+      res.json({
+        message: "Encounter error while sending email",
+        error,
+        from: msg.from,
+        to: msg.to,
+      }).status(500);
     });
 };
 
